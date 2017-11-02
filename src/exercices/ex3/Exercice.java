5d7e2631a5e6e7c761a28fb80cfceb48b1e77926
ricbon35 <ricbon35@gmail.com>
2017-11-02 11:45:32 +0100

    Ajout ex3

---
+++ src/exercices/ex3/Exercice.java
@@ -0,0 +1,10 @@
+package exercices.ex3;
+
+public class Exercice {
+
+	public static void main(String[] args){
+		String s = "1233354856666587777777770000999999";
+		
+		//TODO: faites un algo qui recherche la plus longue séquence d'un même caractère
+	}
+}
